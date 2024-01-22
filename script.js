ba9c1580052a97946580b19a726c9bd878b79754
Urgazieva Aigerim <aigerim.urgazieva.on@gmail.com>
2024-01-22 13:50:56 +0600

    add btn id

--- script.js
+++ script.js
@@ -1,61 +1,63 @@
 let greeting = document.querySelector(".greeting-wrap");
 const greetingBtn = document.querySelector(".greeting-layer__btn");
 let greetingTxt = document.querySelector(".greeting-layer__title");
-let animItems = document.querySelectorAll("._anim-items");
+
 
 // const heroClick = greetingBtn.addEventListener( "click", () => {
 //   greeting.style.display = "none";
 //   console.log("hi")
 // });
 
-//проверяем на наличие элементов, поскольку  queryselectorAll возвращает список узлов из элементов, мы проверяем их с помощью for
-if(animItems.length > 0) {  
-  window.addEventListener('scroll', animOnscroll)
-//
-  function animOnscroll() {
-    for(let index = 0; index < animItems.length; index++){
-  //получаем каждый элемент из массива     
-      const animItem = animItems[index];
-  //получаем высоту объекта     
-      const animItemHeight = animItem.offsetHieght;
-  //получаем позицию объекта относительно вверха, тоесть на сколько объект находится ниже вверха страницы 
-      const animItemOffset = offset(animItem).top;
-  //тут пишем коефициент с какого уровня видения начинать анимацию 
-      const animStart = 4;
+//создаем инстанс обсервер и конструктор IntersectonObserver и нам нужно передать некую коллбек функция и некие опции
 
-//настройка момента старта анимации
-      //с высоты окна браузера отнимаем высоту объекта и делим на наш коефициет
-      let animStartPoint = window.innerHeight -  animItemHeight / animStart;
-    //в случае если высота анимируемого объекта выше высоты окна браузера  
-      if(animItemHeight > window.innerHeight) {
-        animStartPoint = window.innerHeight - window.innerHeight / animStart 
-      }
+//этот блок не работает когда я делаю querySelectoAll и не работает if
+// const options = {
+// }
 
-    //если кол-во пикселей прокрученный в окне больше высоты объекта относительно окна минус точка старта и кол-во пикселей прокрученный в окне меньше высоты объекта относительно окна плюс высота объекта то вызываем класс анимации    
-      if((scrollY > animItemOffset - animStartPoint) && scrollY < (animItemOffset + animItemHeight)) {
-        animItem.classList.add('._activ');
-      } else {
-        animItem.classList.remove('._activ');
-      }
-    }
-  } 
-// получаем корректное значение высоты страницы относительно объекта
-  function offset(el) {
-    const rect = el.getBoundingClientRect(),
-    scrollLeft = window.scrollX || document.documentElement.scrollLeft,
-    scrollTop = window.scrollY || document.documentElement.scrollTop;
-    return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
-  }
-  animOnscroll();
-}
+// const skillObserver = function (entries, observer){
+//     entries.forEach( skill  => {
+   
+//      console.log(skill);      
+//      console.log("done")
+
+//      if(skill.isIntersectin){
+//       target.classList.add(".skills__wrap-list_bt")
+//       console.log('work')
+//      }
+//     });
+//   };
+
+//   const observer = new IntersectionObserver(skillObserver, options)
+
+//   let target = document.querySelector(".skills__wrap");
+//   observer.observe(target)
+//до сюда
+ 
+//теперь попробуеи создать еше один intersection
+// const observer = new IntersectionObserver((entries) => {
+//   entries.forEach((entry) => {
+//     console.log(entry)
+//     if(entry.isIntersecting) {
+//       entry.target.classList.add('.show')
+//       console.log('work')
+//     } else {
+//       entry.target.classList.remove('.show')
+//     }
+//   })
+// })
+
+// const animItems = document.querySelector('.anim');
+// observer.observe(animItems)
+// animItems.forEach((el) => observer.observe(el))
 
 
-const btnPrev = document.querySelector(".projects__hr-scroll-btn_prev").addEventListener("click", () => {
+
+const btnPrev = document.getElementById("prev-btn").addEventListener("click", () => {
   const left = document.querySelector(".projects__hr-scroll-slider").scrollLeft -= 350
   console.log('lol1')
 });
 
-const btnNext = document.querySelector(".projects__hr-scroll-btn_next").addEventListener("click", () => {
+const btnNext = document.getElementById("next-btn").addEventListener("click", () => {
   const right = document.querySelector(".projects__hr-scroll-slider").scrollLeft += 350
   console.log('lol2')
 });
@@ -73,7 +75,7 @@ const btnNext = document.querySelector(".projects__hr-scroll-btn_next").addEvent
 //   }
 // });
 
-//Я бы сделал массив слов и переменную которая считала с какого индекса надо брать слово.
+// Я бы сделал массив слов и переменную которая считала с какого индекса надо брать слово.
 // сделать функцию которая будет каждые 2сек брать слово с массива и выводить его 
 
 // let i = 0;
